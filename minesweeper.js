37d7559f994c1affce6b3a9da613dbcd5c98240c
Eliott Ham <eliotthham@gmail.com>
2017-08-30 22:26:53 -0700

    Board reveals square on click and finds neighboring squares if clicked square is not touching a bomb.

--- minesweeper.js
+++ minesweeper.js
@@ -1,7 +1,24 @@
 $(document).ready(function() {
 	var grid = makeGrid(9);
-
 	render(grid);
+
+
+$('#grid').on('mousedown', '.box', function(event) {
+	switch(event.which) {
+		case 1:
+			if($(this).data('object') instanceof Bomb) {
+				$(this).data('object').show = true;
+			}
+			else {
+				$(this).data('object').showSquare(grid);
+			}
+			render(grid);
+			break;
+		case 3:
+			console.log('mb2');
+
+		}
+	});
 
 });
 
@@ -10,6 +27,7 @@ var surrounding = [ [-1, 1], [0, 1],   [1, 1],  [-1, 0],
 
 function makeGrid(size) {
 	var grid = new Array(size);
+	var mines = Math.floor((size * size) / 3.6);
 	
 	for(var i = 0; i < size; i++) {
 		grid[i] = new Array(size);
@@ -18,7 +36,7 @@ function makeGrid(size) {
 		}
 	}
 	
-	for(var i = 0; i < 11; i++) {
+	for(var i = 0; i < mines; i++) {
 		var x = Math.floor(Math.random() * size);
 		var y = Math.floor(Math.random() * size);
 		grid[x][y] = new Bomb();
@@ -36,26 +54,33 @@ function makeGrid(size) {
 }
 
 function render(grid) {
+	$('#grid').empty();
 	for(var i = 0; i < grid.length; i++) {
 		for(var j = 0; j <= grid.length; j++) {
 			if(j == grid.length) {
 				$('#grid').append('<div class="row"></div>');
 			}
 			else { 
-				$('#grid').append('<div class="box">' + grid[i][j].touching + '</div>')
+				var $box = $('<div class="box"></div>');
+				$box.data('object', grid[i][j]);
+				grid[i][j].show == true ? $box.html(grid[i][j].touching) : '';
+				$('#grid').append($box);
 			}
 		}
 	}
 }
 
 function Bomb() {
+	this.prototype = new Square();
 	this.active = true;
-	this.touching = 'B';
+	this.show = false;
+	this.touching = '*';
 }
 
 function Square(position) {
 	this.position = position;
 	this.touching = 0;
+	this.show = false;
 }
 
 Square.prototype.findBombs = function(grid) {
@@ -64,9 +89,25 @@ Square.prototype.findBombs = function(grid) {
 		var x = that.position[0] + coord[0];
 		var y = that.position[1] + coord[1];
 		if((x >= 0 && x < grid.length) && (y >= 0 && y < grid.length)) {
-			if(grid[x][y].constructor === Bomb) {
+			if(grid[x][y] instanceof Bomb) {
 				that.touching++;
 			}
 		}
 	});
+};
+
+Square.prototype.showSquare = function(grid) {
+	this.show = true;
+	if(this.touching == 0) {
+		var that = this;
+		surrounding.forEach(function(coord) {
+			var x = that.position[0] + coord[0];
+			var y = that.position[1] + coord[1];
+			if((x >= 0 && x < grid.length) && (y >= 0 && y < grid.length)) {
+				if(grid[x][y] instanceof Square && grid[x][y].show == false) {
+					return grid[x][y].showSquare(grid);
+				}
+			}
+		});
+	}
 };
